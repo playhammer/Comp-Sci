77c03c8f89b415f3ca95bbb559c2b9e816d5d955
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-10-19 23:12:13 -0500

    Finished mini assignment 2

--- Com S 227/Workspace/Homework4/src/mini2/HipHipArray.java
+++ Com S 227/Workspace/Homework4/src/mini2/HipHipArray.java
@@ -4,7 +4,12 @@ import java.util.*;
 
 public class HipHipArray {
 	
-	static int[] convertListToArray(List<Integer> l)
+	private HipHipArray()
+	{
+		
+	}
+	
+	private static int[] convertListToArray(List<Integer> l)
 	{
 		int[] temp = new int[l.size()];
 		for(int i = 0; i < l.size(); i++)
@@ -12,7 +17,7 @@ public class HipHipArray {
 		return temp;
 	}
 	
-	static int[] createPalindrome(int[] arr)
+	public static int[] createPalindrome(int[] arr)
 	{
 		List<Integer> result = new ArrayList<Integer>();
 		
@@ -27,7 +32,7 @@ public class HipHipArray {
 		return convertListToArray(result);
 	}
 	
-	static boolean isPermutation(int[] arr)
+	public static boolean isPermutation(int[] arr)
 	{
 		List<Integer> test = new ArrayList<Integer>();
 		
@@ -41,10 +46,11 @@ public class HipHipArray {
 		return true;
 	}
 	
-	static int[] longestRun(int[] arr)
+	public static int[] longestRun(int[] arr)
 	{
 		List<Integer> result = new ArrayList<Integer>(), result_temp = new ArrayList<Integer>();
-		for(int i = 0; i < arr.length; i++)
+		result.add(arr[0]);
+		for(int i = 0; i < arr.length - 1; i++)
 		{
 			if(arr[i] <= arr[i + 1])
 			{
@@ -62,26 +68,100 @@ public class HipHipArray {
 					
 					for(Integer j : result_temp)
 						result.add(j);
-					
-					result_temp.clear();
 				}
+				
+				result_temp.clear();
 			}
+		}
+		
+		if(result_temp.size() > result.size())
+		{
+			if(!result.isEmpty())
+				result.clear();
+			
+			for(Integer j : result_temp)
+				result.add(j);
 		}
 		
 		return convertListToArray(result);
 	}
 	
-	static int[] makeHistogram(double[] data, int numBins, double min, double max)
+	public static int[] makeHistogram(double[] data, int numBins, double min, double max)
 	{
-		List<Double> temp_data = new ArrayList<Double>();
-		for(double d : data)
-			temp_data.add(d);
+		int[] result = new int[numBins];
+		Arrays.fill(result, 0);
 		
-		List<Integer> result = new ArrayList<Integer>();
-		for(int i = 0; i < data.length; i++)
+		for(double d : data)
 		{
-			
+			int index = (int)Math.floor((d - min) / ((max - min) / (double)(numBins)));
+			if(index < numBins && index >= 0)
+				result[index]++;
 		}
+		
+		return result;
+	}
+	
+	public static boolean[] makeSieve(int size, int[] divisors)
+	{
+		boolean[] result = new boolean[size];
+		Arrays.fill(result, true);
+		
+		result[0] = false;
+		
+		for(int j : divisors)
+		{
+			for(int i = 2; i <= (size / j) && (i * j) < size; i++)
+			{
+				if(j < size && !result[j])
+					break;
+				result[i * j] = false;
+			}
+		}
+		
+		return result;
+	}
+	
+	public static String[] removeDups(String[] str)
+	{
+		if(str.length == 0)
+			return str;
+		
+		ArrayList<String> result = new ArrayList(Arrays.asList(new String[]{str[0]}));
+		for(String s : str)
+		{
+			for(int i = 0; i < result.size(); i++)
+			{
+				if(s.equals(result.get(i)))
+					break;
+				else if (i == result.size() - 1)
+					result.add(s);
+			}
+		}
+		return result.toArray(new String[result.size()]);
+	}
+	
+	public static void rotate(int[] arr, int amount)
+	{
+		ArrayList<Integer> result = new ArrayList<>();
+		for(int i : arr)
+			result.add(i);
+		Collections.rotate(result, amount);
+		for(int i = 0; i < arr.length; i++)
+			arr[i] = result.get(i);
 	}
 
+	public static void shift(int[] arr, int amount)
+	{
+	
+		if(Math.abs(amount) >= arr.length)
+			Arrays.fill(arr, 0);
+		else
+		{
+			rotate(arr, amount);
+			for(int i = 0; i < Math.abs(amount); i++)
+				arr[((amount > 0) ? i : (arr.length - 1 - i))] = 0;
+		}
+		
+	}
+	
 }
